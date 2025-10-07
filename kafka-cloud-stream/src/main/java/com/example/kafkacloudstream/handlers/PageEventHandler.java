3939f0aba3fbc4378bafe59635b9fe45e8341fe6
HakimMohammed <mohamed.hakim.dev@gmail.com>
2025-10-07 15:43:17 +0100

    first supplier

--- kafka-cloud-stream/src/main/java/com/example/kafkacloudstream/handlers/PageEventHandler.java
+++ kafka-cloud-stream/src/main/java/com/example/kafkacloudstream/handlers/PageEventHandler.java
@@ -4,7 +4,10 @@ import com.example.kafkacloudstream.events.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Component
 public class PageEventHandler {
@@ -12,5 +15,17 @@ public class PageEventHandler {
     @Bean
     public Consumer<PageEvent> pageEventConsumer() {
         return event -> System.out.println("****************\nReceived event: " + event.toString() + "\n****************");
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier() {
+        return () -> {
+            return new PageEvent(
+                    Math.random() > 0.5 ? "Test 1": "Test 2",
+                    Math.random() > 0.5 ? "User 1": "User 2",
+                    new Date(),
+                    10 + new Random().nextInt(1000)
+            );
+        };
     }
 }
