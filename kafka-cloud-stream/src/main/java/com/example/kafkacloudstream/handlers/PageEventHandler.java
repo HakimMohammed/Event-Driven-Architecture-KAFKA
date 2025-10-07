1ad17e3abff8561bd3914dfbdb44174adff4d357
HakimMohammed <mohamed.hakim.dev@gmail.com>
2025-10-07 15:57:28 +0100

    KStream , get stream from supplier , do something, send it to consumer

--- kafka-cloud-stream/src/main/java/com/example/kafkacloudstream/handlers/PageEventHandler.java
+++ kafka-cloud-stream/src/main/java/com/example/kafkacloudstream/handlers/PageEventHandler.java
@@ -1,12 +1,15 @@
 package com.example.kafkacloudstream.handlers;
 
 import com.example.kafkacloudstream.events.PageEvent;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
@@ -27,5 +30,11 @@ public class PageEventHandler {
                     10 + new Random().nextInt(1000)
             );
         };
+    }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> pageEventFunction() {
+        return stream -> stream.filter((k,v) -> v.duration() > 100)
+                .map((k,v) -> new KeyValue<>(v.name(), v.duration()) );
     }
 }
