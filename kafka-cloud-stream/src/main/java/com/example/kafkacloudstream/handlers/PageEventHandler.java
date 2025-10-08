507098b029241ff584458e00a35822e7df037b6c
HakimMohammed <mohamed.hakim.dev@gmail.com>
2025-10-08 14:30:03 +0100

    Visualise Kafka Stream in FrontEnd

--- kafka-cloud-stream/src/main/java/com/example/kafkacloudstream/handlers/PageEventHandler.java
+++ kafka-cloud-stream/src/main/java/com/example/kafkacloudstream/handlers/PageEventHandler.java
@@ -1,11 +1,17 @@
 package com.example.kafkacloudstream.handlers;
 
 import com.example.kafkacloudstream.events.PageEvent;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -24,8 +30,8 @@ public class PageEventHandler {
     public Supplier<PageEvent> pageEventSupplier() {
         return () -> {
             return new PageEvent(
-                    Math.random() > 0.5 ? "Test 1": "Test 2",
-                    Math.random() > 0.5 ? "User 1": "User 2",
+                    Math.random() > 0.5 ? "Page 1" : "Page 2",
+                    Math.random() > 0.5 ? "User 1" : "User 2",
                     new Date(),
                     10 + new Random().nextInt(1000)
             );
@@ -34,7 +40,13 @@ public class PageEventHandler {
 
     @Bean
     public Function<KStream<String, PageEvent>, KStream<String, Long>> pageEventFunction() {
-        return stream -> stream.filter((k,v) -> v.duration() > 100)
-                .map((k,v) -> new KeyValue<>(v.name(), v.duration()) );
+        return stream ->
+                stream.filter((k, v) -> v.duration() > 100)
+                        .map((k, v) -> new KeyValue<>(v.name(), v.duration()))
+                        .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                        .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                        .count(Materialized.as("page-event-count"))
+                        .toStream()
+                        .map((k, v) -> new KeyValue<>(k.key(), v));
     }
 }
